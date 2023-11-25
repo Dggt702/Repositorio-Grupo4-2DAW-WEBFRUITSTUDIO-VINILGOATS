e1fc459ab23b2608996a94bbc37e8a5611dd3ffe
javix65 <javierhs2003@gmail.com>
2023-11-25 22:48:25 +0100

    deshecho los cambios del commit anterior

--- Avance WebFruit Web/functions.js
+++ Avance WebFruit Web/functions.js
@@ -1,43 +1,80 @@
-document.getElementById('registroForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener("click", validar);
 
-    // Realizar validación de campos aquí antes de enviar el formulario
-    if (validarFormulario()) {
-        location.href = "Page2.html" ;
-        // Aquí puedes enviar los datos al servicio web utilizando AJAX o Fetch
-    }
-});
-
-function validarFormulario() {
-    // Realizar validación de cada campo aquí
-    // Puedes mostrar mensajes de error y cambiar el estilo de los campos si es necesario
-
+function validarFormulario() 
+{
     // Ejemplo de validación de contraseña y confirmación de contraseña
     var password = document.getElementById('password').value;
     var confirmPassword = document.getElementById('confirmPassword').value;
 
     if (password !== confirmPassword) {
-        alert('Las contraseñas no coinciden');
+        error(elemento, "Las contraseñas no coinciden");
         return false;
     }
 
     // Validación adicional para la contraseña según tus criterios
     var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!regex.test(password)) {
-        alert('La contraseña no cumple con los requisitos');
+        error(elemento, "La contraseña no cumple con los requisitos");
         return false;
     }
 
     return true;
-};
+}
 
-function mostrarMas() {
-    // Obtén el elemento oculto
-    var infoOculta = document.getElementById("infoOculta");
+function validarNombre()
+{
+    let elemento = document.getElementById("nombre");
+    let regexN = /^[a-zA-Z]+(?: [a-zA-Z]+)*$/;
+    let errorN = document.getElementById("errorN");
+    if (!regexN.test(elemento.value)) 
+    {
+       errorN.innerHTML = "El nombre no cumple con los requisitos";
+    }
+    if (elemento.value === "")
+    {
+        errorN.innerHTML = "Debe introducir su nombre.";
+        return false;
+    }
+    else if (elemento.validity.patternMismatch) 
+    {
+        errorN.innerHTML = "Formato incorrecto.";
+        return false;
+    }
+    return true;
+}
 
-    // Cambia la propiedad hidden al contrario de su estado actual
-    infoOculta.hidden = !infoOculta.hidden;
+function validarEmail()
+{
+    let elemento = document.getElementById("correo");
+    let regexC = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let errorC = document.getElementById("errorC");
+    if (!regexC.test(elemento.value)) 
+    {
+       errorC.innerHTML = "El correo electrónico no cumple con los requisitos";
+    }
+    if (elemento.value === "")
+    {
+        errorC.innerHTML = "Debe introducir un email válido.";
+        return false;
+    }
+    else if (elemento.validity.patternMismatch) 
+    {
+        errorC.innerHTML = "Formato incorrecto. Debe introducir un email válido.";
+        return false;
+    }
+    return true;
+}
 
-    // Oculta el botón después de mostrar los campos adicionales
-    document.getElementById("mostrarMas").hidden = true;
-};
+function validar(e)
+{
+    if(validarNombre() && validarEmail() && validarFormulario() && confirm("¿Seguro que quiere enviar el formulario?"))
+    {
+        location.href = "Page2.html";
+        return true;
+    }
+    else
+    {
+        e.preventDefault();
+        return false;
+    }
+}
