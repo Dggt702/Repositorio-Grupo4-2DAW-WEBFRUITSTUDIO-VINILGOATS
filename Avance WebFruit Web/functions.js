8ae15b16840fd24c1beab325619ea1b73d65788d
dante <dioggovr702@gmail.com>
2023-11-26 12:52:13 +0100

    COMMIT BY: DIOGGO 12:52 - 26/11/2023

--- Avance WebFruit Web/functions.js
+++ Avance WebFruit Web/functions.js
@@ -1,12 +1,40 @@
+
 document.getElementById('registroForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Realizar validación de campos aquí antes de enviar el formulario
     if (validarFormulario()) {
-        location.href = "Page2.html" ;
+        nombreUsuario = document.getElementById("nombreCampo").value;
+        localStorage.setItem("userRegister",true);
+        localStorage.setItem("nUser",nombreUsuario);
+        location.href = "Page2.html";
+        
+        
         // Aquí puedes enviar los datos al servicio web utilizando AJAX o Fetch
     }
 });
+function desloguear(){
+    localStorage.clear();
+    location.href = "Page2.html";
+}
+
+function confirmacion(){
+    userName = localStorage.getItem("nUser");
+
+    if(localStorage.length===0){
+        document.getElementById("usuarioFR").style.display="flex";
+        document.getElementById("usuarioTR").style.display="none";
+    }else{
+        document.getElementById("usuarioFR").style.display="none";
+        document.getElementById("usuarioR").innerHTML=userName;
+    }
+}
+
+
+function cargar(){
+    confirmacion();
+    slider();
+}    
 
 function validarFormulario() {
     // Realizar validación de cada campo aquí
@@ -29,4 +57,60 @@ function validarFormulario() {
     }
 
     return true;
+}
+
+function slider(){
+
+const btnDerecho = document.querySelector("#btnSliderDerecha");
+const btnIzquierdo = document.querySelector("#btnSliderIzquierda");
+
+let sliderInner=document.querySelector(".slider");
+let images = sliderInner.querySelectorAll("img");
+let index = 1;
+
+function slideAutomatico(){
+    let percentage = index * -100;
+    sliderInner.style.transform = "translateX("+percentage+"%)";
+    index++;
+
+    if(index>images.length - 1){
+        index=0;
+        
+    }
+}
+
+function slideManualIzquierda(){
+      clearInterval(intervalo);
+    
+    let percentage = index * -100;
+    index--;
+    sliderInner.style.transform = "translateX("+percentage+"%)";
+    if(index<=0){
+        index=1;
+    }
+    intervalo=setInterval(slideAutomatico,5000);
+}
+
+function slideManualDerecha(){
+    clearInterval(intervalo);
+    let percentage = index * -100;
+    sliderInner.style.transform = "translateX("+percentage+"%)";
+    index++;
+    if(index>images.length-1){
+        index=0;
+    }
+    if(index<=0){
+        index=1;
+    }
+    intervalo=setInterval(slideAutomatico,5000);
+}
+
+
+
+var intervalo=setInterval(slideAutomatico,5000);
+btnDerecho.addEventListener("click",slideManualDerecha);
+btnIzquierdo.addEventListener("click",slideManualIzquierda);
+
+
+
 }
