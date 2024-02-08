bd64f0742bda769db5877ea5c8bb4d26c95d3686
Claudia Atkinson Bedia <115951660+ClaudiaAtkinsonBedia@users.noreply.github.com>
2024-02-08 16:41:35 +0100

    Avanzado bastante con el carrito pero encontrado error raro - Clau 08/02/2024

--- Avance WebFruit Web/JavaScript/functions.js
+++ Avance WebFruit Web/JavaScript/functions.js
@@ -466,18 +466,62 @@ function slider(){
         {
             containerCartProducts.classList.toggle("hidden-cart");
         });
+
+        const cartInfo = document.querySelector(".cart-product");
+        const rowProduct = document.querySelector(".row-product"); 
+
+        // lista de todos los contenedores de productos
+        const productList = document.querySelector(".container-vinilos");
+
+        // array de productos
+        let allProducts  = [];
+
+        productList.addEventListener("click", e =>
+        {
+            if(e.target.classList.contains("btn-add-cart"))
+            {
+                const product = e.target.parentElement;
+                const infoProduct = 
+                {
+                    quantity: 1,
+                    title: product.querySelector("a").textContent,
+                    price: product.querySelector("p").textContent,
+                }
+                allProducts = [...allProducts, infoProduct];
+                showHTML();
+            }
+        });
+
+        const showHTML = () =>
+        {
+            rowProduct.innerHTML  = "";
+            allProducts.forEach(product => 
+                {
+                    const containerProduct = document.createElement('div');
+                    containerProduct.classList.add("cart-product");
+                    containerProduct.innerHTML = 
+                    `<div class="info-cart-product">
+                        <span class="cantidad-producto-carrito">${product.quantity}</span>
+                        <p class="titulo-producto-carrito">${product.title}</p>
+                        <span class="precio-producto-carrito">${product.price}</span>
+                    </div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="icon-close"
+                    >
+                        <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                        >
+                        </path>
+                    </svg>
+                    `
+                    rowProduct.append(containerProduct);
+                });
+        }
     });
-    
-    const cartInfo = document.querySelector(".cart-product");
-    const rowProduct = document.querySelector(".row-product"); 
-
-    // lista de todos los contenedores de productos
-    const productList = document.querySelector(".container-items");
-
-    // array de productos
-    let allProducts  = [];
-
-    productList.addEventListener("click", e =>
-    {
-        console.log(e);
-    })
